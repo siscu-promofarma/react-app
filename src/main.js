9487cd342fce5318122bdb413040adcba6ecce78
Siscu Cano <francisco.cano@promocionesfarma.com>
2019-06-12 15:57:04 +0200

    First execution

---
+++ src/main.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import { render } from 'react-dom'
+
+import './main.scss'
+
+const Home = () => (
+<div>
+<h1>Test PARCEL</h1>
+</div>
+  )
+render (
+  <Home />,
+  document.getElementById('app')
+  )
